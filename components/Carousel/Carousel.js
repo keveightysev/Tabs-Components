f349457392c4735f76809fe24b46e537587d5ebe
Kevin Smith <keveightysev@gmail.com>
2019-01-31 16:31:44 -0600

    Got prev and next buttons to work

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,11 +1,30 @@
-class Image {
-    constructor(image) {
-        this.image = image;
-    }
-    
+const images = document.querySelectorAll('.image');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-const images = document.querySelectorAll('.item');
-const next = document.querySelector('.next');
-const prev = document.querySelector('.prev')
-images.forEach(image => new Image(image));
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i = 0;
+    if (n > images.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = images.length
+    }
+    images.forEach(image => image.style.display = "none");
+    images[slideIndex-1].style.display = "block";
+}
+
+
+next.addEventListener('click', event => plusSlides(-1));
+prev.addEventListener('click', event => plusSlides(1));
